cb1623698069e5425c6ac0d6802b62cb7a5aad75
s0D3ep <94964262+waaashed@users.noreply.github.com>
2024-01-16 13:18:40 +0100

    Add files via upload

---
+++ STEGANOGRAPHY/script.py
@@ -0,0 +1,25 @@
+# Importer la classe Image du module Pillow
+
+# Définir une fonction pour récupérer le message caché dans une image
+    # Charger l'image depuis le chemin spécifié
+    # Obtenir les pixels de l'image
+    
+    # Initialiser une chaîne pour stocker le message binaire
+    # Initialiser l'indice du pixel
+    
+    # Parcourir les pixels pour récupérer le message binaire
+        # Obtenir le pixel actuel
+        
+        # Extraire le bit de poids faible du canal de couleur (pixel[-1])
+        
+        # Ajouter le bit au message binaire
+        # Incrémenter l'indice du pixel
+    
+    # Convertir le message binaire en texte
+    # Retourner le message
+
+# Spécifier le chemin de l'image modifiée avec le message caché
+
+# Appeler la fonction pour récupérer le message caché
+
+# Afficher le message caché
